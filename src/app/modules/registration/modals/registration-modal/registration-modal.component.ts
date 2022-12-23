e0a42f33003dde65fe4e54677d6b2be1e78bab5c
Ibrahim-Khan88 <ebrahimkhanobak@gmail.com>
2022-12-23 18:36:45 +0600

    Implemented idNumber unique

--- src/app/modules/registration/modals/registration-modal/registration-modal.component.ts
+++ src/app/modules/registration/modals/registration-modal/registration-modal.component.ts
@@ -14,7 +14,9 @@ export class RegistrationModalComponent implements OnInit {
   registrationForm: FormGroup;
   isCreateMode: boolean;
   registrationData: any;
-  idTypes: ['NID', 'PASSPORT', 'DRIVING_LICENCE', 'TRADE_LICENCE']
+  idTypes: ['NID', 'PASSPORT', 'DRIVING_LICENCE', 'TRADE_LICENCE'];
+  isIdNumberExist: boolean = false;
+
   constructor(
     public dialogRef: MatDialogRef<RegistrationModalComponent>,
     private fb: FormBuilder,
@@ -23,7 +25,8 @@ export class RegistrationModalComponent implements OnInit {
 
   ngOnInit(): void {
     this.initialIzeForm();
-    this.checkUniqueidentifier();
+    this.checkUniqueIdentifier();
+    this.checkUniqueIdNumber();
     this.isCreateMode = this.data.registrationData == undefined;
     this.registrationData = this.data.registrationData;
   }
@@ -64,13 +67,11 @@ export class RegistrationModalComponent implements OnInit {
     });
   }
 
-  checkUniqueidentifier(){
+  checkUniqueIdentifier(){
     this.registrationForm.get('identifier').valueChanges.pipe(debounceTime(800)).subscribe(
       data => {
-        console.log('data :>> ', data);
         if (data)
           this.registratioService.checkUniqueidentifier(data).subscribe(isExist => {
-            console.log('isExist :>> ', isExist);
             if(this.registrationForm.get('identifier').hasError('uniqueValueError')){
               delete this.registrationForm.get('identifier').errors['uniqueValueError'];
               this.registrationForm.get('identifier').updateValueAndValidity();
@@ -78,6 +79,20 @@ export class RegistrationModalComponent implements OnInit {
             if(isExist){
               this.registrationForm.get('identifier').setErrors({uniqueValueError: true});
             }
+          }, error => {
+          });
+      }
+    )
+  }
+
+  checkUniqueIdNumber(){
+    this.registrationForm.get('idNumber').valueChanges.pipe(debounceTime(800)).subscribe(
+      data => {
+        console.log('data :>> ', data);
+        if (data)
+          this.registratioService.checkUniqueIdNumber(data).subscribe(isExist => {
+            console.log('isExist :>> ', isExist);
+              this.isIdNumberExist = isExist;
           }, error => {
            // console.log('error :>> ', error);
           });
--- src/app/shared/constant/baseUrl.ts
+++ src/app/shared/constant/baseUrl.ts
@@ -1,1 +1,2 @@
-export const accountServiceBaseUrl = ' http://103.125.252.81:8080/account/';
+export const accountServiceBaseUrl = 'http://103.125.252.81:8080/account/';
+export const kycServiceBaseUrl = 'http://localhost:8090/';
